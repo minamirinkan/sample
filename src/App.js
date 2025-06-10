35cc368b6a7b2c652cd3d7be9eb9a56db01fcb86
Masahiro Tomo <basilisk.mshr@icloud.com>
2025-06-10 18:20:42 +0900

    ルートの追加

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SuperAdminLogin from './pages/SuperAdminLogin';
 import SuperAdminDashboard from "./pages/SuperAdminDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 import AdminLogin from './pages/AdminLogin';
 import TeacherLogin from './pages/TeacherLogin';
 import CustomerLogin from './pages/CustomerLogin';
 import StudentLogin from './pages/StudentLogin';
 import TimetablePage from './pages/TimetablePage';
 import CalendarPopup from './components/CalendarPopup';
+import DevLoginSelector from './pages/DevLoginSelector';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SuperAdminLogin />} />
+        <Route path="/" element={<DevLoginSelector />} />
         <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/superadmin-login" element={<SuperAdminLogin />} />
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/teacher-login" element={<TeacherLogin />} />
         <Route path="/customer-login" element={<CustomerLogin />} />
